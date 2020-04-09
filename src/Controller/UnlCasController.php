6848df7cf2f57940553377a27586242020ee5352
Christopher Burge <cburge2@unl.edu>
2020-04-09 11:00:11 -0500

    [#12] Remove Deprecated code; prepare for D9; add UnlCasAdapter service

--- src/Controller/UnlCasController.php
+++ src/Controller/UnlCasController.php
@@ -3,64 +3,56 @@
 namespace Drupal\unl_cas\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Routing\RedirectDestination;
 use Drupal\Core\Session\AnonymousUserSession;
 use Drupal\Core\Url;
+use Drupal\unl_cas\UnlCasAdapter;
 use Drupal\unl_user\Helper;
 use Drupal\user\Entity\User;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class UnlCasController extends ControllerBase {
 
-  public $adapter;
+  /**
+   * A UNL CAS adapter.
+   *
+   * @var \Drupal\unl_cas\UnlCasAdapter
+   */
+  protected $unlCasAdapter;
 
-  static $zendLoaded = FALSE;
+  /**
+   * Provides helpers for redirect destinations.
+   *
+   * @var \Drupal\Core\Routing\RedirectDestination
+   */
+  protected $redirectDestination;
 
-  public function unl_load_zend_framework() {
-    if (UnlCasController::$zendLoaded) {
-      return;
-    }
-
-    set_include_path(get_include_path() . PATH_SEPARATOR . dirname(__FILE__) . '/../../libraries');
-    require_once 'Zend/Loader/Autoloader.php';
-    $autoloader = \Zend_Loader_Autoloader::getInstance();
-    $autoloader->registerNamespace('Unl_');
-    UnlCasController::$zendLoaded = TRUE;
+  /**
+   * Constructs a UnlCasController object.
+   *
+   * @param \Drupal\unl_cas\UnlCasAdapter $unl_cas_adapter
+   *   A UNL CAS adapter.
+   * @param \Drupal\Core\Routing\RedirectDestination $redirect_destination
+   *   Provides helpers for redirect destinations.
+   */
+  public function __construct(UnlCasAdapter $unl_cas_adapter, RedirectDestination $redirect_destination) {
+    $this->unlCasAdapter = $unl_cas_adapter;
+    $this->redirectDestination = $redirect_destination;
   }
 
-  public function getAdapter() {
-    $this->unl_load_zend_framework();
-
-    /**
-     * @var \Symfony\Component\HttpFoundation\Session\Session $session
-     */
-    $session = \Drupal::service('session');
-
-    // Start the session because if drupal doesn't then Zend_Session will.
-    if (!$session->isStarted()) {
-      /**
-       * Drupal will start a 'lazy' session for anonymous users so that a cookie is not set (to help with things like varnish)
-       * We can't $session->start(), because it is already lazy-started
-       * Instead we need to migrate it to a stored (non-lazy) session
-       */
-      $session->migrate();
-    }
-
-    if (!$this->adapter) {
-      if (\Drupal::request()->isSecure()) {
-        $url = Url::fromRoute('unl_cas.validate', array(), array('absolute' => TRUE, 'query' => drupal_get_destination(), 'https' => TRUE))->toString();
-      } else {
-        $url = Url::fromRoute('unl_cas.validate', array(), array('absolute' => TRUE, 'query' => drupal_get_destination()))->toString();
-      }
-      $this->adapter = new \Unl_Cas($url, 'https://shib.unl.edu/idp/profile/cas');
-    }
-
-    \Drupal::request()->query->remove('destination');
-
-    return $this->adapter;
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('unl_cas.adapter'),
+      $container->get('redirect.destination')
+    );
   }
 
   public function validate() {
-    $cas = $this->getAdapter();
+    $cas = $this->unlCasAdapter->getAdapter();
 
     if (array_key_exists('logoutRequest', $_POST)) {
       $cas->handleLogoutRequest($_POST['logoutRequest']);
@@ -88,7 +80,7 @@ class UnlCasController extends ControllerBase {
       setcookie('unl_sso', 'fake', time() - 60 * 60 * 24, '/', '.unl.edu');
     }
 
-    $destination = drupal_get_destination();
+    $destination = $this->redirectDestination->getAsArray();
     if (!$destination['destination']) {
       $destination['destination'] = 'admin';
     }
