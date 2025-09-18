11145972e769f9c0deda56e465c470cc00137768
Eric Rasmussen <ericrasmussen1@gmail.com>
2025-09-18 18:11:31 -0500

    Add UnlCasRedirectSubscriber.php
    The domain module in 2.0.0-beta3 introduced DomainRedirectResponseSubscriber:checkRedirectUrl which runs before CasLoginRedirectSubscriber:onRedirectResponse which has a priority of 5.

---
+++ src/Subscriber/UnlCasRedirectSubscriber.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Drupal\unl_cas\Subscriber;
+
+use Drupal\cas\Subscriber\CasLoginRedirectSubscriber;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * Listens to KernelEvents::RESPONSE events.
+ */
+class UnlCasRedirectSubscriber extends CasLoginRedirectSubscriber {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents(): array {
+    return [
+        KernelEvents::RESPONSE => [
+          // Act before DomainRedirectResponseSubscriber::checkRedirectUrl() which has a
+          // lower priority (10).
+          // @see \Drupal\Core\EventSubscriber\DomainRedirectResponseSubscriber::checkRedirectUrl()
+            ['onRedirectResponse', 20],
+        ],
+    ];
+  }
+
+}
+
